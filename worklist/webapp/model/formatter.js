998085c8fca984c2e47316ff87c2a41b3936a3b1
Jakob <jakob@agilux.com.au>
2021-05-13 22:46:03 +1000

    Jakob changes

--- worklist/webapp/model/formatter.js
+++ worklist/webapp/model/formatter.js
@@ -15,6 +15,13 @@ sap.ui.define([], function () {
 			}
 
 			return parseFloat(sValue).toFixed(2);
+		},
+
+		getImage: function(sText){
+			debugger
+		},
+		formatDate: function(sDate){
+			debugger;
 		}
 	};
 });
